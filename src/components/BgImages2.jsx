ce44e6bbd6e2da9d48915524375f4a1957a77ab1
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2022-08-29 11:05:15 +0530

    added second page and fixed menu appearence bug

--- src/components/BgImages2.jsx
+++ src/components/BgImages2.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 import "./styles/BgImages2.css";
+import { Carousel } from "react-responsive-carousel";
+import Solar_roof from "../assets/images/Solar_roof.jpg";
+import Giga_tesla from "../assets/images/Giga_tesla.jpg";
+import PowerWall_tesla from "../assets/images/PowerWall_tesla.jpg";
+import tesla_roadster from "../assets/images/Roadster_tesla.jpg";
+import model_S from "../assets/images/Model_S.jpg";
+import model_X from "../assets/images/Model_X.jpg";
 
 function BgImages2() {
-  return <div className="bg2">BgImages2</div>;
+  return (
+    <div className="bg2">
+      <Carousel
+        className="main_slide_2"
+        showArrows={false}
+        showThumbs={false}
+        autoPlay={true}
+        interval={5000}
+        showStatus={false}
+        infiniteLoop={true}
+        stopOnHover={false}
+        dynamicHeight={true}
+        swipeable={true}
+        emulateTouch={true}
+      >
+        <div>
+          <img src={Solar_roof} className="main_img" alt="teslaHere" />
+          <a href="https://www.tesla.com/solarpanels" className="legend">
+            Solar Roof
+          </a>
+        </div>
+        <div>
+          <img src={Giga_tesla} className="main_img" alt="teslaHere" />
+          <a href="https://www.tesla.com/gigafactory" className="legend">
+            Giga Factory
+          </a>
+        </div>
+        <div>
+          <img src={PowerWall_tesla} className="main_img" alt="teslaHere" />
+          <a href="https://www.tesla.com/powerwall" className="legend">
+            Power Wall
+          </a>
+        </div>
+      </Carousel>
+    </div>
+  );
 }
 
 export default BgImages2;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,34 @@ import Header_logo from "../assets/logo/Header_logo.png";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IconContext } from "react-icons";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 function Header() {
   const [menu_state, setMenu_state] = useState("Button_sec");
+  const [isHamClicked, setisHamClicked] = useState(false);
 
   const update_menu = () => {
     console.log("clicked");
+    if (isHamClicked) {
+      setMenu_state("Button_sec_clicked");
+    } else {
+      setMenu_state("Button_sec");
+    }
+    setisHamClicked(!isHamClicked);
   };
+
   return (
     <div className="Header">
-      <img src={Header_logo} className="Header_logo" alt="Header logo here" />
-      <div className={menu_state}>
+      <a href="/">
+        <img src={Header_logo} className="Header_logo" alt="Header logo here" />
+      </a>
+      <motion.div
+        className={menu_state}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
         <a href="https://shop.tesla.com/">
           <button>Shop</button>
         </a>
@@ -26,9 +43,9 @@ function Header() {
         <a href="https://www.tesla.com/blog">
           <button>News</button>
         </a>
-      </div>
+      </motion.div>
       <IconContext.Provider value={{ className: "ham_button" }}>
-        <GiHamburgerMenu onClick={update_menu} />
+        <GiHamburgerMenu onClick={update_menu} id="ham_trigger" />
       </IconContext.Provider>
     </div>
   );
